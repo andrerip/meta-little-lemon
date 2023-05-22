8f09012c3759528417ab7a122ccd909dc1068dfc
André Ripari <andrerip@gmail.com>
2023-05-22 11:20:21 -0300

    new nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import { Box, HStack } from "@chakra-ui/react";
+import Nav from './Nav';
 
 const logo = require('../images/logo.png');
 
@@ -8,19 +9,8 @@ const header = () => {
             <Box top={0} left={0} right={0} backgroundColor="white">
                 <Box>
                     <HStack>
-                        <nav>
-                            <img src={logo} alt="logo" />
-                        </nav>
-                        <nav>
-                            <HStack spacing={40}>
-                                <a href="#home" className="navBarLink menu-text">HOME</a>
-                                <a href="#about" className="navBarLink menu-text">ABOUT</a>
-                                <a href="#menu" className="navBarLink menu-text">MENU</a>
-                                <a href="#reserv" className="navBarLink menu-text">RESERVATIONS</a>
-                                <a href="#order" className="navBarLink menu-text">ORDER ONLINE</a>
-                                <a href="#login" className="navBarLink menu-text">LOGIN</a>
-                            </HStack>
-                        </nav>
+                        <img src={logo} alt="logo" />
+                        <Nav />
                     </HStack>
                 </Box>
             </Box>
